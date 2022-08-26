0349faaa34bae53b16243164d41cb6574d056f1f
INUKO <loli@prprpr.love>
2022-08-26 15:53:47 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 #Set PATH
 
-resName="wyx176"
+resName="showrbq"
 serviceFile=/etc/opt/ss5/service.sh
 newVersionMsg=`curl -s -L  https://raw.githubusercontent.com/${resName}/Socks5/master/update.txt`
 
 echo ""
-echo "1.启动"
-echo "2.停止"
-echo "3.重启"
-echo "4.状态"
-echo "5.更新"
-echo "6.卸载"
-echo "0.返回"
+echo "1.Start"
+echo "2.Stop"
+echo "3.Restart"
+echo "4.Status"
+echo "5.Update"
+echo "6.Uninstall"
+echo "0.Exit"
 while :; do echo
-	read -p "请选择： " choice
+	read -p "Enter: " choice
 	if [[ ! $choice =~ ^[0-6]$ ]]; then
-		echo "输入错误! 请输入正确的数字!"
+		echo "Unknown choice!"
 	else
-		break	
+		break
 	fi
 done
-
 
 if [[ $choice == 0 ]];then
 	s5
 fi
-	
+
 if [[ $choice == 1 ]];then
-clear
-if [ ! -d "/var/run/ss5/" ];then
-mkdir /var/run/ss5
-fi
-service ss5 start
-  bash $serviceFile
+    clear
+    if [ ! -d "/var/run/ss5/" ];then
+        mkdir /var/run/ss5
+    fi
+    service ss5 start
+    bash $serviceFile
 fi
 
 if [[ $choice == 2 ]];then
@@ -54,15 +53,14 @@ if [[ $choice == 4 ]];then
 	bash $serviceFile
 fi
 
-
 if [[ $choice == 5 ]];then
 	clear
-	echo "更新到最新版本将丢失所有数据,请提前备份！"
+	echo "You will lose all the program data after update, please backup first!"
 	echo " "
-	echo "最新版本："
+	echo "New Version: "
 	echo -e ${newVersionMsg}
 	echo " "
-	read -p "输入123开始更新,其它则取消： " c
+	read -p "Enter 123 to continue: " c
 	if [[ "$c" == "123" ]];then
 	wget -q -N --no-check-certificate https://raw.githubusercontent.com/${resName}/Socks5/master/install.sh && bash install.sh
 	exit 0
@@ -72,29 +70,27 @@ if [[ $choice == 5 ]];then
 	fi
 fi
 
-
-
 if [[ $choice == 6 ]];then
 	clear
-	echo "你在做什么？你真的这么狠心吗？"
-	read -p "输入886开始卸载,其它则取消： " c
+	echo "Are you sure you want to uninstall the program?"
+	read -p "Enter 886 to continue: " c
 	if [[ "$c" == "886" ]];then
-	service ss5 stop
-	rm -rf /run/ss5
- rm -f 	/run/lock/subsys/ss5
-	rm -rf /etc/opt/ss5
-	rm -f /usr/local/bin/s5
- rm -rf 	/usr/lib/ss5
-rm -f /usr/sbin/ss5
-rm -rf /usr/share/doc/ss5
-rm -rf /root/ss5-3.8.9
-rm -f /etc/sysconfig/ss5
-rm -f /etc/rc.d/init.d/ss5
-rm -f /etc/pam.d/ss5
-rm -rf /var/log/ss5
-	clear
-	echo "Socks5服务卸载完毕！"
-	exit 0
+        service ss5 stop
+        rm -rf /run/ss5
+        rm -f  /run/lock/subsys/ss5
+        rm -rf /etc/opt/ss5
+        rm -f  /usr/local/bin/s5
+        rm -rf /usr/lib/ss5
+        rm -f  /usr/sbin/ss5
+        rm -rf /usr/share/doc/ss5
+        rm -rf /root/ss5-3.8.9
+        rm -f  /etc/sysconfig/ss5
+        rm -f  /etc/rc.d/init.d/ss5
+        rm -f  /etc/pam.d/ss5
+        rm -rf /var/log/ss5
+        clear
+        echo "Uninstall Fininshed."
+        exit 0
 	else
 		bash $serviceFile
 	fi
--- user.sh
+++ user.sh
@@ -10,106 +10,109 @@ echo "*******************************"
 cat $confFile | while read line
 do
 if  [[ $line =~ "permit u	0.0.0.0/0	-	0.0.0.0/0	-	-	-	-	-" ]] ;then
-echo  "@帐号验证：已开启@"
-break
+    echo  "@Account Verification: Enabled@"
+    break
 fi
 
 if  [[ $line =~ "permit -	0.0.0.0/0	-	0.0.0.0/0	-	-	-	-	-" ]] ;then
-echo "@帐号验证：已关闭@"
-break
+    echo "@Account Verification: Disabled@"
+    break
 fi
+
 done
 echo "*******************************"
-echo "1.查看用户"
-echo "2.添加用户"
-echo "3.删除用户"
-echo "4.开启账号验证"
-echo "5.关闭账号验证"
-echo "6.修改端口"
-echo "0.返回"
+echo "1.List Accounts"
+echo "2.Add Account"
+echo "3.Delete Account"
+echo "4.Enable Account Verification"
+echo "5.Disable Account Verification"
+echo "6.Edit Port Number"
+echo "0.Exit"
 while :; do echo
-	read -p "请选择： " choice
+	read -p "Enter: " choice
 	if [[ ! $choice =~ ^[0-6]$ ]]; then
-		echo "输入错误! 请输入正确的数字!"
+		echo "Unknown choice!"
 	else
-		break	
+		break
 	fi
 done
+
 if [[ $choice == 0 ]];then
-s5
+    s5
 fi
 
 if [[ $choice == 1 ]];then
-clear
-echo "用户名	密码"
-cat $passwdFile | while read line
-do
-echo "$line" #输出整行内容
-#echo "$line" | awk '{print $1}' #输出每行第一个字
-done
-bash $userfile
+    clear
+    echo "User	Password"
+    cat $passwdFile | while read line
+    do
+        echo "$line" #输出整行内容
+        #echo "$line" | awk '{print $1}' #输出每行第一个字
+    done
+    bash $userfile
 fi
 
 if [[ $choice == 2 ]];then
-clear
-cd /etc/opt/ss5
+    clear
+    cd /etc/opt/ss5
 
-read -p "输入用户名： " uname
-echo ""
-read -p "输入密码： " upasswd
-echo ""
-echo -e $uname $upasswd >> ss5.passwd
-echo "*添加用户成功*"
-bash $userfile
+    read -p "Enter User Name: " uname
+    echo ""
+    read -p "Enter Password: " upasswd
+    echo ""
+    echo -e $uname $upasswd >> ss5.passwd
+    echo "*Success!*"
+    bash $userfile
 fi
 
 if [[ $choice == 3 ]];then
-clear
-read -p "输入用户名：" uname
-echo ""
-sed -i -e "/$uname/d" $passwdFile
-echo "      执行成功,重启s5后生效"
-bash $userfile
+    clear
+    read -p "Enter User Name: " uname
+    echo ""
+    sed -i -e "/$uname/d" $passwdFile
+    echo "*Success! Work after restart s5.*"
+    bash $userfile
 fi
+
 if [[ $choice == 4 ]];then
-clear
-sed -i '87c auth    0.0.0.0/0               -               u' $confFile
-sed -i '203c permit u	0.0.0.0/0	-	0.0.0.0/0	-	-	-	-	-' $confFile
-echo ""
-var=`service ss5 restart`
-if [[ $var =~ "OK" ]] ;then
- echo "开启账户验证成功"
- else
- echo "设置失败,请重试"
- fi
-bash $userfile
+    clear
+    sed -i '87c auth    0.0.0.0/0               -               u' $confFile
+    sed -i '203c permit u	0.0.0.0/0	-	0.0.0.0/0	-	-	-	-	-' $confFile
+    echo ""
+    var=`service ss5 restart`
+    if [[ $var =~ "OK" ]] ;then
+        echo "Enable Verification: Success!"
+        else
+        echo "Enable Verification: Failed!"
+    fi
+    bash $userfile
 fi
 
 if [[ $choice == 5 ]];then
-clear
-sed -i '87c auth    0.0.0.0/0               -               -' $confFile
-sed -i '203c permit -	0.0.0.0/0	-	0.0.0.0/0	-	-	-	-	-' $confFile
-echo ""
-var=`service ss5 restart`
-if [[ $var =~ "OK" ]] ;then
- echo "账户验证关闭成功！"
- else
- echo "设置失败,请重试"
- fi
-bash $userfile
+    clear
+    sed -i '87c auth    0.0.0.0/0               -               -' $confFile
+    sed -i '203c permit -	0.0.0.0/0	-	0.0.0.0/0	-	-	-	-	-' $confFile
+    echo ""
+    var=`service ss5 restart`
+    if [[ $var =~ "OK" ]] ;then
+        echo "Disable Verification: Success!"
+        else
+        echo "Disable Verification: Failed!"
+    fi
+    bash $userfile
 fi
 
 if [[ $choice == 6 ]];then
-clear
-read -p "请输入新端口：" port
-sed -i '2c SS5_OPTS="-u root -b 0.0.0.0:' $portFile
-sed -i "/0.0.0:/ s/$/$port\"/" $portFile
-echo ""
-var=`service ss5 restart`
-if [[ $var =~ "OK" ]] ;then
- echo "端口修改成功"
- else
- echo "设置失败,请重试"
- fi
-bash $userfile
+    clear
+    read -p "Enter A New Port: " port
+    sed -i '2c SS5_OPTS="-u root -b 0.0.0.0:' $portFile
+    sed -i "/0.0.0:/ s/$/$port\"/" $portFile
+    echo ""
+    var=`service ss5 restart`
+    if [[ $var =~ "OK" ]] ;then
+        echo "Edit Port: Success!"
+        else
+        echo "Edit Port: Failed!"
+    fi
+    bash $userfile
 fi
